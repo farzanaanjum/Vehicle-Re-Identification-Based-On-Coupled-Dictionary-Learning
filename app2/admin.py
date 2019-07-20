fd35f65a84938a77aa1a8eab943c11734988eae4
Farzana Anjum <30755050+farzanaanjum@users.noreply.github.com>
2019-07-20 01:41:37 -0600

    admin file
    admin file

---
+++ app2/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from models import CrimeFill
+# Register your models here.
+from models import CrimeFill
+from models import Databse
+from models import AllVehicle
+from models import UpdateCrime
+
+admin.site.register(CrimeFill)
+admin.site.register(AllVehicle)
+admin.site.register(Databse)
+admin.site.register(UpdateCrime)
